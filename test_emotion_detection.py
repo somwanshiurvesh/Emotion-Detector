0e56351f0532d083fc16e5af136deba71fbca7c7
somwanshiurvesh <166993078+somwanshiurvesh@users.noreply.github.com>
2026-04-08 02:06:38 +0530

    Refactor tests for emotion detector functionality

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,11 +1,25 @@
 import unittest
-from EmotionDetection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
-class TestEmotionDetector(unittest.TestCase):
+class TestEmotionDetection(unittest.TestCase):
 
-    def test_joy(self):
+    def test_emotion_detector_output_keys(self):
         result = emotion_detector("I am happy")
-        self.assertEqual(result['dominant_emotion'], 'joy')
 
-if __name__ == '__main__':
+        self.assertIsInstance(result, dict)
+
+        self.assertIn('anger', result)
+        self.assertIn('disgust', result)
+        self.assertIn('fear', result)
+        self.assertIn('joy', result)
+        self.assertIn('sadness', result)
+        self.assertIn('dominant_emotion', result)
+
+    def test_dominant_emotion(self):
+        result = emotion_detector("I am very happy")
+
+        self.assertIn(result['dominant_emotion'], ['anger', 'disgust', 'fear', 'joy', 'sadness'])
+
+
+if __name__ == "__main__":
     unittest.main()
