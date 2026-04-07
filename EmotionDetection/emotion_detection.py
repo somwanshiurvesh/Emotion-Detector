07e35cfe169f75b890742eaada02dd19ce2819a6
somwanshiurvesh <166993078+somwanshiurvesh@users.noreply.github.com>
2026-04-08 00:36:13 +0530

    Refactor emotion detection response handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,7 +3,9 @@ import requests
 def emotion_detector(text_to_analyse):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
 
     input_json = {
         "raw_document": {
@@ -14,17 +16,15 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json=input_json, headers=headers)
 
     if response.status_code == 200:
-        formatted_response = response.json()
-        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        response_dict = response.json()
+        emotions = response_dict['emotionPredictions'][0]['emotion']
 
         return {
             'anger': emotions['anger'],
             'disgust': emotions['disgust'],
             'fear': emotions['fear'],
             'joy': emotions['joy'],
-            'sadness': emotions['sadness'],
-            'dominant_emotion': max(emotions, key=emotions.get)
+            'sadness': emotions['sadness']
         }
-
-    elif response.status_code == 400:
+    else:
         return None
